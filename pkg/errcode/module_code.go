37531ca7c90a51259387721d8a02de7ff705faf7
猪猪酱 <2664006323@qq.com>
2022-05-17 23:04:40 +0800

    修复登录接口

--- pkg/errcode/module_code.go
+++ pkg/errcode/module_code.go
@@ -1,7 +1,7 @@
 package errcode
 
 var (
-	ErrorLoginFail       = NewError(10010001, "登录失败")
+	ErrorLoginFail       = NewError(10010001, "用户名或密码错误")
 	ErrorLoginExpire     = NewError(10010002, "登录过期")
 	ErrorRegisterFail    = NewError(10010003, "注册失败")
 	ErrorGetUserInfoFail = NewError(10010004, "获取用户信息失败")
