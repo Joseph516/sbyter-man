2e58601c8e0b915303d5c21c7808e8f4b87d0aab
1506182898 <1506182898@qq.com>
2022-05-22 16:33:30 +0800

    避免重复的关注

--- internal/dao/follow.go
+++ internal/dao/follow.go
@@ -1,13 +1,27 @@
 package dao
 
-import "douyin_service/internal/model"
+import (
+	"douyin_service/internal/model"
+)
 
-func (d *Dao) FollowAction(follower, followed int64) error {
+func (d *Dao) CreateFollow(follower, followed int64) error {
 	follow := model.Follow{
 		FollowedId: followed,
 		FollowerId: follower,
 	}
-
+	if flag, err:= follow.IsExist(d.engine);flag{
+		return err
+	}
 	err := follow.Create(d.engine)
+	return err
+}
+
+func (d *Dao) CancelFollow(follower, followed int64) error {
+
+	follow := model.Follow{
+		FollowedId: followed,
+		FollowerId: follower,
+	}
+	err := follow.Delete(d.engine)
 	return err
 }
--- internal/model/follow.go
+++ internal/model/follow.go
@@ -1,6 +1,9 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"gorm.io/gorm"
+)
 
 type Follow struct {
 	gorm.Model
@@ -14,4 +17,18 @@ func (f Follow) TableName() string {
 
 func (f Follow) Create(db *gorm.DB) error {
 	return db.Create(&f).Error
+}
+
+func (f Follow) Delete(db *gorm.DB) error {
+	return db.Where("followed_id = ? And follower_id = ?", f.FollowedId, f.FollowerId).Delete(&f).Error
+}
+
+func (f Follow) IsExist(db *gorm.DB ) (bool, error) {
+	var follows []Follow
+	db.Where("followed_id = ? and follower_id = ?", f.FollowedId, f.FollowerId).Find(&follows)
+	fmt.Println(follows)
+	if len(follows)==0{
+		return false, nil
+	}
+	return true, nil
 }
