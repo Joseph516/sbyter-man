f1467a3d66baacaa3d44c3d600882b7145f96350
1506182898 <1506182898@qq.com>
2022-05-22 17:06:22 +0800

    fetch user_id from token

--- internal/service/follow.go
+++ internal/service/follow.go
@@ -3,19 +3,19 @@ package service
 import "douyin_service/pkg/errcode"
 
 type FollowActionRequest struct {
-	UserId int64   `form:"user_id"  binding:"required"`
+	//UserId int64   `form:"user_id"  binding:"required"`
 	Token  string `form:"token" binding:"required"`
 	ToUserId int64 `form:"to_user_id" binding:"required"`
 	ActionType int64 `form:"action_type" binding:"required"`
 
 }
 
-func (svc *Service) FollowAction(param *FollowActionRequest) error {
+func (svc *Service) FollowAction(param *FollowActionRequest, user_id int64) error {
 	switch param.ActionType{
 	case 1:
-		return svc.dao.CreateFollow(param.UserId, param.ToUserId)
+		return svc.dao.CreateFollow(user_id, param.ToUserId)
 	case 2:
-		return svc.dao.CancelFollow(param.UserId, param.ToUserId)
+		return svc.dao.CancelFollow(user_id, param.ToUserId)
 	default:
 		return errcode.InvalidParams
 	}
